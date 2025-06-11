0b9c73679699e1aa6e4bdd6b5728642e2e44190a
Nicole <nicolejardim.c@gmail.com>
2025-06-11 14:44:44 -0300

    Add files via upload

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,13 +4,15 @@ import { ProfileComponent } from "./components/profile/profile.component";
 import { TimelineComponent } from "./components/timeline/timeline.component";
 import { LoginComponent } from "./components/pages/login/login.component";
 import { SignupComponent } from "./components/pages/signup/signup.component";
+import { UserListComponent } from "./components/user-list/user-list.component";
 
 const routes: Routes = [
     { path: 'timeline', component: TimelineComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent},
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: '' },
+    { path: 'users/:id', component: UserListComponent}
 
 ];
 
